a2f51a892e9dd92361854c39a398e697e302ca14
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2024-12-02 20:09:25 -0800

    Added priority Queue and B-ary Heap solutions to part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -4,7 +4,10 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use priority_queue::PriorityQueue;
 use adv_code_2024::*;
+use dary_heap::DaryHeap;
+
 
 const DAY: &str = "01";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
@@ -41,13 +44,60 @@ fn main() -> Result<()> {
         Ok(answer)
     }
 
-    // TODO: Set the expected answer for the test input
-    assert_eq!(11, part1(BufReader::new(TEST.as_bytes()))?);
+    fn part1_pq<R: BufRead>(reader: R) -> Result<i32> {
+        let mut l = PriorityQueue::new();
+        let mut r = PriorityQueue::new();
+        reader.lines().flatten().enumerate().for_each(|(i,line)| {
+            let mut parts = line.split_whitespace();
+            // parse the two numbers and append them to the l and r lists
+            let left: i32 = parts.next().unwrap().parse().unwrap();
+            let right: i32 = parts.next().unwrap().parse().unwrap();
+            l.push(i, left);
+            r.push(i, right);
+        });
+        let answer = l.into_sorted_iter().zip(r.into_sorted_iter()).map(|((_,a), (_,b))| (a - b).abs()).sum();
+        Ok(answer)
+    }
+    fn part1_heap<const N:usize, R: BufRead>(reader: R) -> Result<i32> {
+        let mut l = DaryHeap::<_, N>::new();
+        let mut r = DaryHeap::<_, N>::new();
+        reader.lines().flatten().enumerate().for_each(|(i,line)| {
+            let mut parts = line.split_whitespace();
+            // parse the two numbers and append them to the l and r lists
+            let left: i32 = parts.next().unwrap().parse().unwrap();
+            let right: i32 = parts.next().unwrap().parse().unwrap();
+            l.push(left);
+            r.push(right);
+        });
+        let answer = l.into_iter_sorted().zip(r.into_iter_sorted()).map(|(a,b)| (a - b).abs()).sum();
+        Ok(answer)
+    }
+
+
+assert_eq!(11, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
     println!("Result = {}", result);
     //endregion
+
+    println!("\n=== Priority Queue ===");
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1_pq(input_file)?);
+    println!("Result = {}", result);
+
+    println!("\n=== Heap 2 ===");
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1_heap::<2,_>(input_file)?);
+    println!("Result = {}", result);
+    println!("\n=== Heap 5 ===");
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1_heap::<5,_>(input_file)?);
+    println!("Result = {}", result);
+    println!("\n=== Heap 10 ===");
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1_heap::<10,_>(input_file)?);
+    println!("Result = {}", result);
 
     //region Part 2
     println!("\n=== Part 2 ===");
